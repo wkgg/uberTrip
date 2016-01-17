bc512f592f5a9d5d2e3f9da69eee9635a20dfab7
ouyang.jianhua <ouyang.jianhua@immomo.com>
2016-01-17 17:16:39 +0800

    add time price file

---
+++ time_price_test.js
@@ -0,0 +1,73 @@
+var request = require('request');
+var getTimePrice = function( callback ){
+
+    var x=39.979785,y=116.310304;//中关村
+    var sx=40.005254,sy=116.486406;//望京
+
+    var headers = {
+		'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicmVxdWVzdCJdLCJzdWIiOiI0YWZmN2UwZC0zNWEyLTRlN2UtYmYxNy1jODA2NjIwYjgxNjUiLCJpc3MiOiJ1YmVyLWNuMSIsImp0aSI6ImNkNGRiNDk1LThjYTQtNDY1ZS04Mjc2LTY0OTYyMTFlY2Q1OCIsImV4cCI6MTQ1NTUzNTY4NiwiaWF0IjoxNDUyOTQzNjg1LCJ1YWN0IjoiaUF0RnZiSXBSeDhOTUg5dWdsU2xsMXNpVU40c0h0IiwibmJmIjoxNDUyOTQzNTk1LCJhdWQiOiJjbUxETWtaNGNyOXJpdkdsUGQ4WjZBUU0tZ2R3aVUydCJ9.sXyIREP46oSdj__RcNh0lv4D6huWMhTDc31WymHN1VpqfSE_ayGmVCHJ7ZxgpWf9oPfCjIQvCsLUgbYCVH-fhDJ5KxCrIRk18P3H-kILto7cdFuEXUddefcRdfS8YXUz47K5gwCNR7JOqHmrFe5MozX-H7BMICzBDU549_1CEgsx8pgl5E6n6ecw7_FgDdYBx5LezuAqJT2bmPF8JaLTVeuVEI7CGAeJujgciTfamof-pNDFKu7PMgiV7qQCAPCWb_g7ygoMya-Z6NBdLHdi1cd7AL8ld10T4gI8JVvF1FeZpwBsFDzosIde7XiEW6YLmJiXjVihbk-aLmKV7JWSZA',
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	};
+
+    var api = 'https://sandbox-api.uber.com.cn/v1/estimates/';
+
+    var list = {
+    	price : {
+    		start_latitude:sx,
+    		start_longitude:sy,
+    		end_latitude:x,
+    		end_longitude:y
+    	},
+    	time :{
+    		start_latitude:sx,
+    		start_longitude:sy,
+            end_latitude:x,
+            end_longitude:y
+    	}
+    }
+
+    var price_query = [];
+    var time_query = list.time;
+
+    for(var i in list.price){
+    	price_query.push( i+'='+list.price[i] );
+    }
+
+
+    request({
+        url: api + 'price?' + price_query.join('&'),
+        headers : headers
+    },function(error, response, result){
+    	
+    	result = JSON.parse(result);
+
+    	time_query.product_id = result.prices[0]['product_id'];
+
+    	request.post({
+            url: 'https://sandbox-api.uber.com.cn/v1/requests/estimate',
+            headers : headers,
+            json : time_query
+        },function(error, response, res){
+
+    		var time = res && res.trip && res.trip.duration_estimate || '';
+            var estimate = result && result.prices[0] && result.prices[0]['estimate'] || '';
+            var display_name = result && result.prices[0] && result.prices[0]['display_name'] || '';
+
+            callback.call(null,{
+                'time': Math.floor(time/60+0.99999) + '分钟',
+                'estimate' : estimate.replace('CN',''),
+                'display_name' : display_name
+            });
+
+            console.log('time: ',Math.floor(time/60+0.99999) + '分钟', ' estimate: ' , estimate.replace('CN',''), ' display_name: ', display_name);
+
+        })
+    })
+
+}
+exports.getTimePrice = getTimePrice;
+
+//getTimePrice(function(data){
+//    console.log(data);
+//});
